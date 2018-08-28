5075295292f3bd9e6c70fe039121eba6d95ee181
Poyan <Poyan.delavari@gmail.com>
2018-08-28 09:52:10 +0200

    1

---
+++ Datasheets/Datasheets/Customers.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Customers] (
+	[CostumerNumber] INT   IDENTITY (1, 1) NOT NULL,
+    [FirstName]     NVARCHAR (50) NULL,
+    [LastName]      NVARCHAR (50) NULL,
+    [Email]			NVARCHAR (50) NULL,
+	[PhoneNumber]	INT Null,
+    [Domains]		NVARCHAR(50) NULL, 
+    [InvestCap] MONEY NULL, 
+    PRIMARY KEY CLUSTERED ([CostumerNumber] ASC)
+)
---
+++ Datasheets/Datasheets/CustomersPDS.sql
@@ -0,0 +1,11 @@
+﻿MERGE INTO Customers AS Target 
+USING (VALUES 
+        (1, 'Ali', 'Delavari', 'Ali.delavari@test.com', 00848485854, 'Tech',7000000), 
+        (2, 'Jack', 'Jonson', 'Jack.jonson@test.com', 00185485418, 'Medcare',85000000), 
+        (3, 'Tom', 'Jingelberry', 'Tj@test.com', 00485148525, 'DomainX',950000000)
+) 
+AS Source (CostumerNum, FirstName, LastName, Email, PhoneNumber, Domain, InvestCap) 
+ON Target.CostumerNum = Source.CostumerNum 
+WHEN NOT MATCHED BY TARGET THEN 
+INSERT (FirstName, LastName, Email, PhoneNumber, Domain, InvestCap) 
+VALUES (FirstName, LastName, Email, PhoneNumber, Domain, InvestCap);
